2df4db49e9837e17ad04022002b29110340039cb
cph-jj395 <32088963+cph-jj395@users.noreply.github.com>
2018-02-22 14:32:32 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,123 @@
+<div id="flexboksAM">	
+	
+
+	
+	<div class="containerAM">
+  		<img class="imageAM" src="http://kristjansson.dk/mynasesati/alex_largeBW.jpg" alt="Billede af Alexander Amir Zatuna Jakobsen" >
+  		<div class="overlayAM" >
+      		<div class="textAM" >
+				<h1 class="h1AM">Alexander Amir Zatuna Jakobsen</h1>
+				<p>Fodboldspiller: IFK Norrköping, Egyptens landshold</p>
+				<p>Instagram: @alexanderjakobsen_</p>
+	  		</div>
+  		</div>
+	</div>
+	
+	
+	<div class="containerAM">
+  		<img class="imageAM" src="http://kristjansson.dk/mynasesati/Younes_namli_mediumBW.jpg" alt="Billede af Younes Namli">
+  		<div class="overlayAM">
+      		<div class="textAM">
+				<h1 class="h1AM">Younes Namli</h1>
+				<p>Fodboldspiller: PEC Zwolle</p>
+				<p>Instagram: @younesnamli</p>
+	  		</div>
+  		</div>
+	</div>
+
+	
+	<div class="containerAM">
+  		<img class="imageAM" src="http://kristjansson.dk/mynasesati/Mikkelwolf_largeBW.jpg" alt="Billede af Mikkel Wolf">
+  		<div class="overlayAM">
+      		<div class="textAM">
+				<h class="h1AM"1>Mikkel Wolf</h1>
+				<p>Streamer</p>
+				<p>Twitch: @Mikkelwolf YouTube: @Mikkelwolf</p>
+	  		</div>
+  		</div>
+	</div>
+	
+	
+	<div class="containerAM">
+  		<img class="imageAM" src="http://kristjansson.dk/mynasesati/Kasper_svare_largeBW.jpg" alt="Billede af Kasper Svare">
+  		<div class="overlayAM">
+      		<div class="textAM">
+				<h1 class="h1AM">Kasper Svare</h1>
+				<p>Musiker</p>
+				<p>Instagram: @kaspersvare Facebook/kaspersvaremusic</p>
+	  		</div>
+  		</div>
+	</div>
+	
+	
+	<div class="containerAM">
+  		<img class="imageAM" src="http://kristjansson.dk/mynasesati/Stine_haslev_largeBW.jpg" alt="Billede af Stine Haslev Christensen">
+  		<div class="overlayAM">
+      		<div class="textAM">
+				<h1 class="h1AM">Stine Haslev Christensen</h1>
+				<p>Crossfitter</p>
+				<p>Instagram: @stinehaslevchristensen</p>
+	  		</div>
+  		</div>
+	</div>
+	
+	
+	<div class="containerAM">
+  		<img class="imageAM" src="http://kristjansson.dk/mynasesati/Sander_schmidt_mediumBW.jpg" alt="Billede af Sander Schmidt">
+  		<div class="overlayAM">
+      		<div class="textAM">
+				<h1 class="h1AM">Sander Schmidt</h1>
+				<p>Crossfitter</p>
+				<p>Instagram: @Sander Schmidt</p>
+	  		</div>
+  		</div>
+	</div>
+	
+
+	<div class="containerAM">
+  		<img class="imageAM" src="http://kristjansson.dk/mynasesati/Nicoline_trasbo_compactBW.jpg" alt="Billede af Nicoline Trasbo Madsen">
+  		<div class="overlayAM">
+      		<div class="textAM">
+				<h1 class="h1AM">Nicoline Trasbo Madsen</h1>
+				<p>Mountainbikerytter</p>
+				<p>Instagram: @nicolinetrasbo</p>
+	  		</div>
+  		</div>
+	</div>
+	
+	
+	<div class="containerAM">
+  		<img class="imageAM" src="http://kristjansson.dk/mynasesati/Daniel_schouw_mediumBW.jpg" alt="Billede af Daniel Schouw Jensen">
+  		<div class="overlayAM">
+      		<div class="textAM">
+				<h1 class="h1AM">Daniel Schouw Jensen</h1>
+				<p>Basketballspiller: BK Amager</p>
+				<p>Instagram: @dsjensen</p>
+	  		</div>
+  		</div>
+	</div>
+	
+
+	
+</div>	
+
+
+
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "wrapper" div and all content after.
+ *
+ * @package Hestia
+ * @since Hestia 1.0
+ */
+?>
+			<?php do_action( 'hestia_do_footer' ); ?>
+		</div>
+	</div>
+<?php wp_footer(); ?>
+
+
+</body>
+</html>
